6f56a3b8a6068d6cb4d7bb2cbeb62e408136cea1
Bogdan Stevanovic <bogdan.stevanovic090204@gmail.com>
2025-11-20 04:43:57 +0100

    logo added

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import Image from "next/image";
+import logo from '@/public/logo_outlined.svg'
 
 const Logo = () => {
   return (
     <Link href="/">
-      <h4 className="uppercase opacity-80 hover:opacity-100 transition-all duration-300 ease-in-out">boge.dev</h4>
+      <Image src={logo} alt="Bogdan Stevanovic - Logo" height={64} />
+      {/* <h4 className="uppercase opacity-80 hover:opacity-100 transition-all duration-300 ease-in-out">boge.dev</h4> */}
     </Link>
   );
 };
